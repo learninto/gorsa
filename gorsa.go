1ad4d034cd84dd3ca167a20adcefd19605d3a2c8
learninto <14171158+learninto@users.noreply.github.com>
2023-06-15 14:02:58 +0800

    Revert "1、新增 GenerateKey 生成RSA私钥和公钥  2、解决：解密之后的值 不等于 加密前的值"

--- gorsa.go
+++ gorsa.go
@@ -5,80 +5,66 @@ import (
 	"encoding/hex"
 )
 
-// PublicEncrypt 公钥加密
+// 公钥加密
 func PublicEncrypt(data, publicKey string) (string, error) {
 
-	gRsa := RSASecurity{}
-	gRsa.SetPublicKey(publicKey)
+	grsa := RSASecurity{}
+	grsa.SetPublicKey(publicKey)
 
-	rsaData, err := gRsa.PubKeyENCTYPT([]byte(data))
+	rsadata, err := grsa.PubKeyENCTYPT([]byte(data))
 	if err != nil {
 		return "", err
 	}
 
-	baseData := base64.StdEncoding.EncodeToString(rsaData)
-	return hex.EncodeToString([]byte(baseData)), nil
+	return base64.StdEncoding.EncodeToString(rsadata), nil
 }
 
-// PriKeyEncrypt 私钥加密
+// 私钥加密
 func PriKeyEncrypt(data, privateKey string) (string, error) {
 
-	gRsa := RSASecurity{}
-	gRsa.SetPrivateKey(privateKey)
+	grsa := RSASecurity{}
+	grsa.SetPrivateKey(privateKey)
 
-	rsaData, err := gRsa.PriKeyENCTYPT([]byte(data))
+	rsadata, err := grsa.PriKeyENCTYPT([]byte(data))
 	if err != nil {
 		return "", err
 	}
 
-	baseData := base64.StdEncoding.EncodeToString(rsaData)
-	return hex.EncodeToString([]byte(baseData)), nil
+	return base64.StdEncoding.EncodeToString(rsadata), nil
 }
 
-// PublicDecrypt 公钥解密
+// 公钥解密
 func PublicDecrypt(data, publicKey string) (string, error) {
-	dataByte, err := hex.DecodeString(data)
+
+	databs, _ := base64.StdEncoding.DecodeString(data)
+
+	grsa := RSASecurity{}
+	if err := grsa.SetPublicKey(publicKey);err !=nil{
+		return "",err
+	}
+
+	rsadata, err := grsa.PubKeyDECRYPT(databs)
 	if err != nil {
 		return "", err
 	}
-
-	dataBs, err := base64.StdEncoding.DecodeString(string(dataByte))
-	if err != nil {
-		return "", err
-	}
-
-	gRsa := RSASecurity{}
-	if err := gRsa.SetPublicKey(publicKey); err != nil {
-		return "", err
-	}
-
-	rsaData, err := gRsa.PubKeyDECRYPT(dataBs)
-	if err != nil {
-		return "", err
-	}
-
-	return string(rsaData), nil
+	return hex.EncodeToString(rsadata),nil
 }
 
-// PriKeyDecrypt 私钥解密
+// 私钥解密
 func PriKeyDecrypt(data, privateKey string) (string, error) {
-	dataByte, err := hex.DecodeString(data)
+
+	databs, _ := base64.StdEncoding.DecodeString(data)
+
+	grsa := RSASecurity{}
+
+	if err := grsa.SetPrivateKey(privateKey); err != nil {
+		return "", err
+	}
+
+	rsadata, err := grsa.PriKeyDECRYPT(databs)
 	if err != nil {
 		return "", err
 	}
 
-	dataBs, _ := base64.StdEncoding.DecodeString(string(dataByte))
-
-	gRsa := RSASecurity{}
-
-	if err := gRsa.SetPrivateKey(privateKey); err != nil {
-		return "", err
-	}
-
-	rsaData, err := gRsa.PriKeyDECRYPT(dataBs)
-	if err != nil {
-		return "", err
-	}
-
-	return string(rsaData), nil
+	return hex.EncodeToString(rsadata), nil
 }
